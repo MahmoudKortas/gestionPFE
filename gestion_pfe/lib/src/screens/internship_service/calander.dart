a49e01a2b51ef8f1866ea532675076134b480f3e
Mahmoud <36514895+MahmoudKortas@users.noreply.github.com>
2022-05-20 12:00:34 +0100

    change calander

--- gestion_pfe/lib/src/screens/internship_service/calander.dart
+++ gestion_pfe/lib/src/screens/internship_service/calander.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:gestion_pfe/src/screens/authentification/signIn.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import '../pages/basics_example.dart';
+import '../pages/complex_example.dart';
+import '../pages/events_example.dart';
+import '../pages/multi_example.dart';
+import '../pages/range_example.dart';
 
 /// Displays detailed information about a SampleItem.
 class Calander extends StatelessWidget {
@@ -12,92 +16,53 @@ class Calander extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SfCalendar(
-      view: CalendarView.month,
-      dataSource: MeetingDataSource(_getDataSource()),
-      // by default the month appointment display mode set as Indicator, we can
-      // change the display mode as appointment using the appointment display
-      // mode property
-      monthViewSettings: const MonthViewSettings(
-          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20.0),
+          ElevatedButton(
+            child: Text('Basics'),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => TableBasicsExample()),
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          ElevatedButton(
+            child: Text('Range Selection'),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => TableRangeExample()),
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          ElevatedButton(
+            child: Text('Events'),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => TableEventsExample()),
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          ElevatedButton(
+            child: Text('Multiple Selection'),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => TableMultiExample()),
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          ElevatedButton(
+            child: Text('Complex'),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => TableComplexExample()),
+            ),
+          ),
+          const SizedBox(height: 20.0),
+        ],
+      ),
     ));
   }
-
-  List<Meeting> _getDataSource() {
-    final List<Meeting> meetings = <Meeting>[];
-    final DateTime today = DateTime.now();
-    final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
-    final DateTime endTime = startTime.add(const Duration(hours: 2));
-    meetings.add(Meeting(
-        'Conference', startTime, endTime, const Color(0xFF0F8644), false));
-    return meetings;
-  }
-}
-
-/// An object to set the appointment collection data source to calendar, which
-/// used to map the custom appointment data to the calendar appointment, and
-/// allows to add, remove or reset the appointment collection.
-class MeetingDataSource extends CalendarDataSource {
-  /// Creates a meeting data source, which used to set the appointment
-  /// collection to the calendar
-  MeetingDataSource(List<Meeting> source) {
-    appointments = source;
-  }
-
-  @override
-  DateTime getStartTime(int index) {
-    return _getMeetingData(index).from;
-  }
-
-  @override
-  DateTime getEndTime(int index) {
-    return _getMeetingData(index).to;
-  }
-
-  @override
-  String getSubject(int index) {
-    return _getMeetingData(index).eventName;
-  }
-
-  @override
-  Color getColor(int index) {
-    return _getMeetingData(index).background;
-  }
-
-  @override
-  bool isAllDay(int index) {
-    return _getMeetingData(index).isAllDay;
-  }
-
-  Meeting _getMeetingData(int index) {
-    final dynamic meeting = appointments![index];
-    late final Meeting meetingData;
-    if (meeting is Meeting) {
-      meetingData = meeting;
-    }
-
-    return meetingData;
-  }
-}
-
-/// Custom business object class which contains properties to hold the detailed
-/// information about the event data which will be rendered in calendar.
-class Meeting {
-  /// Creates a meeting class with required details.
-  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
-
-  /// Event name which is equivalent to subject property of [Appointment].
-  String eventName;
-
-  /// From which is equivalent to start time property of [Appointment].
-  DateTime from;
-
-  /// To which is equivalent to end time property of [Appointment].
-  DateTime to;
-
-  /// Background which is equivalent to color property of [Appointment].
-  Color background;
-
-  /// IsAllDay which is equivalent to isAllDay property of [Appointment].
-  bool isAllDay;
 }
